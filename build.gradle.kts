bf1483cefccb93fbdfba33f058685061e962b7c1
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2024-07-04 17:59:55 +0200

    pom.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,17 @@ plugins {
     id("org.sonarqube") version "5.0.0.4638"
     id("org.cyclonedx.bom") version "1.8.2"
 }
+
+tasks.cyclonedxBom {
+    setIncludeConfigs(listOf("runtimeClasspath"))
+    setSkipConfigs(listOf("compileClasspath", "testCompileClasspath"))
+    setSkipProjects(listOf(rootProject.name, "yourTestSubProject"))
+    setProjectType("application")
+    setSchemaVersion("1.5")
+    setDestination(project.file("build/reports"))
+    setOutputName("bom")
+    setOutputFormat("json")
+    setIncludeBomSerialNumber(false)
+    setIncludeLicenseText(true)
+    setComponentVersion("2.0.0")
+}
